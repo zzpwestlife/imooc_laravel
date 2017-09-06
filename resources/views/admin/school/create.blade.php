bf672e9f327c50b2e28c59df44cd41faf68722d7
Zou Zhipeng <495043143@qq.com>
2017-09-07 00:11:07 +0800

    school

--- resources/views/admin/school/create.blade.php
+++ resources/views/admin/school/create.blade.php
@@ -6,36 +6,36 @@
         <!-- Small boxes (Stat box) -->
         <div class="row">
             <div class="col-lg-10 col-xs-6">
-                <div class="box">
-
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">@if(isset($school->id))编辑学校@else增加学校@endif</h3>
+                    </div>
                     <!-- /.box-header -->
-                    <div class="box box-primary">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">@if(isset($school->id))编辑学校@else增加学校@endif</h3>
-                        </div>
-                        <!-- /.box-header -->
-                        <!-- form start -->
-                        <form role="form" action="/admin/school/store" method="POST" id="form-school">
-                            {{csrf_field()}}
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label for="name">学校名</label>
+                    <!-- form start -->
+                    <form role="form" action="/admin/school/store" method="POST" id="form-item">
+                        {{csrf_field()}}
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label for="name" class="col-sm-2 control-label">学校名</label>
+
+                                <div class="col-sm-4">
                                     <input type="hidden" value="{{$school->id or 0}}" name="id">
-                                    <input type="text" class="form-control" name="name" id="name" minlength="2" required
-                                           value="@if(!empty($school)){{$school->name}}@endif">
+                                    <input type="text" class="form-control" name="name" id="name" minlength="4" required
+                                           value="@if(!empty($school)){{$school->name}}@endif" placeholder="请输入学校名">
                                 </div>
                             </div>
-                        @include('admin.layout.error')
+                        </div>
                         <!-- /.box-body -->
-                            <div class="box-footer">
-                                <a class="btn btn-icon btn-default m-b-5" href="/admin/school"
-                                   title="取消">
-                                    取消
-                                </a>
-                                <button type="submit" class="btn btn-primary">提交</button>
-                            </div>
-                        </form>
-                    </div>
+                        @include("admin.layout.error")
+                        <div class="box-footer" style="margin-left: 160px;">
+                            <a class="btn btn-icon btn-default m-b-5" href="/admin/school"
+                               title="取消">取消
+                            </a>
+                            &emsp;
+                            <button type="submit" class="btn btn-primary">提交</button>
+                        </div>
+                        <!-- /.box-footer -->
+                    </form>
                 </div>
             </div>
         </div>
@@ -45,7 +45,8 @@
 @section('script')
     <script type="text/javascript">
         $(document).ready(function () {
-            $("#form-school").validate();
+            $('#name').focus();
+            $("#form-item").validate();
         });
     </script>
 @endsection
--- resources/views/admin/school/index.blade.php
+++ resources/views/admin/school/index.blade.php
@@ -19,15 +19,15 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-            <div id="school_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            <div id="item_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
                 <div class="row">
                     <div class="col-sm-6"></div>
                     <div class="col-sm-6"></div>
                 </div>
                 <div class="row">
                     <div class="col-sm-12">
-                        <table id="school-table" class="table table-bordered table-hover dataTable" role="grid"
-                               aria-describedby="school_info">
+                        <table id="item-table" class="table table-bordered table-hover dataTable" role="grid"
+                               aria-describedby="item_info">
                             <thead>
                             <tr role="row">
                                 <th style="width: 10px">#</th>
